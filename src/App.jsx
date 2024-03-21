73f2ae736212c07390eda3255cfa1c7f0aca2fb9
tonyroxtar <antonio.chamal@gmail.com>
2024-03-20 23:08:48 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
